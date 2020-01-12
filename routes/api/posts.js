4f51e7b542d97a97db7ba0fd3df73801abc62beb
Not-An-Alien <milesrichie@mit3ch.com>
2020-01-11 19:36:50 -0500

    added routes to server

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-//@route        GET api/users
-//@description  test route 
+//@route        GET api/posts
+//@description  post route 
 //@access       public 
-router.get('/', (req, res) => res.send('User route'));
+router.get('/', (req, res) => res.send('Post route'));
 
 module.exports = router;
