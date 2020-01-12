6c3eea794f2ec6e914ae50156f001ed3c7b46cdf
Not-An-Alien <milesrichie@mit3ch.com>
2020-01-11 20:08:36 -0500

    added checks users

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,7 +8,11 @@ const {
 //@description  Register User 
 //@access       public 
 router.post('/', [
-    check('name', 'Name is required').not().isEmpty()
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please enter a valid email/').isEmail(),
+    check('password', 'Please enter a password with 6 or more charectors').isLength({
+        min: 6
+    })
 ], (req, res) => {
     console.log(req.body);
     res.send('User route');
