c53bee3eb4385d6eed181c2f5f26374e62c23d57
Not-An-Alien <milesrichie@mit3ch.com>
2020-01-11 19:48:48 -0500

    Created user model

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,6 +4,6 @@ const router = express.Router();
 //@route        GET api/auth
 //@description  auth route 
 //@access       public 
-router.get('/', (req, res) => res.send('Post route'));
+router.get('/', (req, res) => res.send('auth route'));
 
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,6 @@ const router = express.Router();
 //@route        GET api/auth
 //@description  auth route 
 //@access       public 
-router.get('/', (req, res) => res.send('Post route'));
+router.get('/', (req, res) => res.send('Profile route'));
 
 module.exports = router;
