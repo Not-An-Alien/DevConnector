de0df7a917d3c1a942e3aedb1db1c8e5a68faaac
Not-An-Alien <milesrichie@mit3ch.com>
2020-01-11 20:03:50 -0500

    added a check to the users route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
+const {
+    check,
+    validationResult
+} = require('express-validator/check');
 //@route        POST api/users
 //@description  Register User 
 //@access       public 
-router.post('/', (req, res) => {
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty()
+], (req, res) => {
     console.log(req.body);
     res.send('User route');
 });
