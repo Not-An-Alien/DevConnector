af7d7a53a2ea82726d3bb008e8d349c01e869db6
Not-An-Alien <milesrichie@mit3ch.com>
2020-01-13 13:58:04 -0500

    added a route in profile to create or update

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const {
+    check,
+    validationResult
+} = require('express-validator');
 
 //import models 
 const profile = require('../../models/Profile');
 const user = require('../../models/User');
+
+
+
+// BELOW ARE THE ROUTES/END POINTS PERTAINING TO PROFILES 
+
+
 
 //@route        GET api/profile/me
 //@description  Get current users profile  
@@ -28,5 +38,24 @@ router.get('/me', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+//@route        POST api/profile
+//@description  Create or update a users profile  
+//@access       private 
+//***NOTE*** WHEN USING MULTIPLE MIDDLE WARES BUT THEM IN BRACKETS
+router.post('/', [auth, [
+    //This is where we put our field checks 
+    check('status', 'Status is required').not().isEmpty(),
+    check('skill', 'skills is requried').not().isEmpty()
+]], async (req, res) => {
+    //This takes in the results from above and stores the results 
+    const errors = validationResult(req);
+    // Here we check for the errors "if errors is not empty print the result"
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array
+        });
+    }
+})
 
 module.exports = router;
