7400ee9cfd95f606d57531857b97ef4e9b6b253d
Not-An-Alien <milesrichie@mit3ch.com>
2020-01-13 15:53:26 -0500

    created route that fetches all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -46,15 +46,92 @@ router.get('/me', auth, async (req, res) => {
 router.post('/', [auth, [
     //This is where we put our field checks 
     check('status', 'Status is required').not().isEmpty(),
-    check('skill', 'skills is requried').not().isEmpty()
+    check('skills', 'skills is requried').not().isEmpty()
 ]], async (req, res) => {
     //This takes in the results from above and stores the results 
     const errors = validationResult(req);
     // Here we check for the errors "if errors is not empty print the result"
     if (!errors.isEmpty()) {
         return res.status(400).json({
-            errors: errors.array
+            errors: errors.array()
         });
+
+    }
+    //Here we pull everything out of the body 
+    const {
+        company,
+        website,
+        location,
+        bio,
+        status,
+        githubusername,
+        skills,
+        youtube,
+        facebook,
+        twitter,
+        instagram,
+        linkedin
+    } = req.body;
+
+    //Build Profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+        profileFields.skills = skills.split(',').map(skill => skill.trim());
+    }
+
+    //Build our social media object 
+    profileFields.social = {}
+
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+
+    try {
+        let profile = await Profile.findOne({
+            user: req.user.id
+        });
+        if (profile) {
+            //update the profile
+            profile = await Profile.findOneAndUpdate({
+                user: req.user.id
+            }, {
+                $set: profileFields
+            }, {
+                new: true
+            });
+            return res.json(profile);
+        }
+        //create if no profile found 
+        profile = new Profile(profileFields);
+        await profile.save();
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+//@route        Get api/profile
+//@description  Get all profiles
+//@access       public
+//***NOTE*** 
+
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
     }
 })
 
