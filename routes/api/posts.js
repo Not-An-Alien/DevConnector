bb12838cdb6f5cd195a4f09521cf7e278a8eb476
Not-An-Alien <milesrichie@mit3ch.com>
2020-01-14 15:45:07 -0500

    created route to get all posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -30,12 +30,12 @@ router.post('/',
 
             const user = await User.findById(req.user.id).select('-password');
 
-            const newPost = {
+            const newPost = new Post({
                 text: req.body.text,
                 name: user.name,
                 avatar: user.avatar,
                 user: req.user.id
-            }
+            });
 
             const post = await newPost.save();
 
@@ -47,5 +47,21 @@ router.post('/',
         }
 
     });
+
+//@route        Get api/posts
+//@description  Get all posts  
+//@access       Private 
+
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({
+            date: -1
+        });
+        res.json(posts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
 
 module.exports = router;
