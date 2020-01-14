8470c035f009b0938029902a9f9502dfe2502934
Not-An-Alien <milesrichie@mit3ch.com>
2020-01-14 16:12:42 -0500

    added delete route for posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -62,6 +62,75 @@ router.get('/', auth, async (req, res) => {
         console.error(err.message);
         res.status(500).send('Server Error');
     }
+});
+
+//@route        GET api/posts/:id
+//@description  get a single post by id  
+//@access       Private
+
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({
+                msg: 'Post not found'
+            })
+        }
+        res.json(post);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({
+                msg: 'Post not found'
+            })
+        }
+        res.status(500).send('Server Error');
+    }
+})
+
+
+
+
+
+//@route        Delete api/posts/:id
+//@description  Delete a post by a user   
+//@access       Private
+
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // check to see if the user exists 
+        if (!post) {
+            return res.status(404).json({
+                msg: "Post Not Found"
+            });
+        }
+
+
+        //Check on user to make sure that they own the post 
+        // This compares the post user to the user sending the request 
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({
+                msg: 'User not authorized'
+            });
+        }
+
+        await post.remove();
+
+        res.json({
+            msg: 'Post Removed'
+        });
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({
+                msg: "Post Not Found"
+            });
+        }
+        res.status(500).send('Server Error');
+    }
 })
 
 module.exports = router;
