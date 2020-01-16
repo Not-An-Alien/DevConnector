35096c4ad5caa7e9980d5dd0aa293c3d138ca539
Not-An-Alien <milesrichie@mit3ch.com>
2020-01-16 18:43:23 -0500

    asd

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,21 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // Bring in components 
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App = () => (
   <Router>
     <Fragment>
       <Navbar />
       <Route exact path='/' component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/Login' component={Login} />
+        </Switch>
+
+      </section>
     </Fragment>
   </Router>
 
