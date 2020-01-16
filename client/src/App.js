bfdfa5c71912bd788ac725167d09beb1d3b37175
Not-An-Alien <milesrichie@mit3ch.com>
2020-01-16 17:33:01 -0500

    Created login and register components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React, { Fragment } from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// Bring in components 
+import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
 
 const App = () => (
-  <Fragment>
-    <h1>App</h1>
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+    </Fragment>
+  </Router>
+
 );
 
 
