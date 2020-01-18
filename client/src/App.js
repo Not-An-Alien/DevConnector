0e8cc663012e72b674012f92f536563b132d86f1
Not-An-Alien <milesrichie@mit3ch.com>
2020-01-18 16:27:56 -0500

    created store and root reducer

--- client/src/App.js
+++ client/src/App.js
@@ -12,22 +12,31 @@ import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+// Redux 
+import { Provider } from 'react-redux';
+import store from './store';
+
+
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className="container" >
-        <Switch >
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/Login' component={Login} />
-        </Switch >
-      </section>
-    </Fragment >
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className="container" >
+          <Switch >
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/Login' component={Login} />
+          </Switch >
+        </section>
+      </Fragment >
+    </Router>
+  </Provider>
 
-)
+
+);
+
 
 
 export default App;
