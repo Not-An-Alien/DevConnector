e341aaa25be4eea34681095c259eec51ecaca7ae
Not-An-Alien <milesrichie@mit3ch.com>
2020-02-13 15:54:45 -0500

    created post reducer

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -8,6 +8,7 @@ import ProfileTop from './ProfileTop';
 import ProfileExperience from './ProfileExperience';
 import ProfileAbout from './ProfileAbout';
 import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 
 const Profile = ({
 	getProfileById,
@@ -69,6 +70,10 @@ const Profile = ({
 								<h4> No education </h4>
 							)}
 						</div>
+
+						{profile.githubusername && (
+							<ProfileGithub username={profile.githubusername} />
+						)}
 					</div>
 				</Fragment>
 			)}
