745bf762e7a5782163ee5282b68f982d3dd659ec
Not-An-Alien <milesrichie@mit3ch.com>
2020-03-02 15:13:50 -0500

    -

--- server.js
+++ server.js
@@ -4,17 +4,13 @@ const path = require('path');
 
 const app = express();
 
-//connect database
+// Connect Database
 connectDB();
 
-//init middleware
-app.use(
-	express.json({
-		extended: false
-	})
-);
+// Init Middleware
+app.use(express.json({ extended: false }));
 
-//define Routes
+// Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
@@ -22,13 +18,14 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-	//set static folder
+	// Set static folder
 	app.use(express.static('client/build'));
 
-	app.length('*', (req, res) => {
+	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server is running ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
